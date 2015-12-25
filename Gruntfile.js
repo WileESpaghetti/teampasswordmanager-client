c0c8dbe0d940e9ac782cf161d72c2ec5079860da
Lehman Black <wileespaghetti@users.noreply.github.com>
2015-12-24 22:38:54 -0600

    Add testing support

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,20 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-  });
+	grunt.initConfig({
+		jasmine: {
+			test: {
+				src: 'src/**/*.js',
+				options: {
+					specs: 'spec/*Spec.js',
+					helpers: 'spec/*Helper.js'
+				}
+			}
+		}
+	});
 
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
+	grunt.loadNpmTasks('grunt-contrib-jasmine');
 
-  grunt.registerTask('default', []);
+	grunt.registerTask('default', []);
+	grunt.registerTask('test', ['jasmine:test']);
 
 };
