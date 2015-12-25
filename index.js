bef0300d3f1cd9bec351390836be8e29aba3cc8d
Lehman Black <wileespaghetti@users.noreply.github.com>
2015-12-24 22:55:12 -0600

    Add config file support

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+var rc = require('rc');
+
 var TeamPasswordManager = function() {
+	var _config;
+
+	_config = rc('teampasswordmanager', {
+		apiVersion: 'v4',
+		baseUrl: '', // trailingslashit
+		user: '',
+		pass: '',
+		weakSsl: false // true allows self signed certs
+	});
 };
 
 module.exports = TeamPasswordManager;
